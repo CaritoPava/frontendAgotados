55b87e84ee48869b505a179bba5fc521640d9c9a
CaritoPava <carito9018@gmail.com>
2022-08-04 19:28:29 -0500

    add css

--- src/App.js
+++ src/App.js
@@ -179,7 +179,7 @@ function App() {
   return (
     <div >
       <div className='conteSelectPDV'>
-        <img src={require('./assets/blanco.png')} alt='logo' className='logoSayo' />
+        {/* <img src={require('./assets/blanco.png')} alt='logo' className='logoSayo' /> */}
         <audio src='./assets/sound/alert.wav' autoPlay={true} loop={true} controls={false} volume={1} />
         <button className='btnSelectPDV' onClick={() => setViewPDV({ ...viewPDV, circunvalar: !viewPDV.circunvalar })}>Circunvalar</button>
         <button className='btnSelectPDV' onClick={() => setViewPDV({ ...viewPDV, treinta: !viewPDV.treinta })}>Av 30 de Agosto</button>
