1f93e5caac1b593c4628a60555cdabfcd6c410f2
Carito Pava <carolina@MacBook-Pro-de-Carito.local>
2022-10-05 10:50:31 -0500

    environment variables are removed and replaced by production server

--- src/App.js
+++ src/App.js
@@ -137,7 +137,7 @@ function App() {
       setIsConnected(false)
     })
 
-    const urlPlaces = `http://3.87.44.241:8080/api/places`
+    const urlPlaces = `http://44.208.37.247:8080/api/places`
 
 
     try {
--- src/helpers/getSoldOut.js
+++ src/helpers/getSoldOut.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export const getSoldOut = async () => {
   // const urlSoldOut = `${process.env.REACT_APP_URL_API || 'localhost:8080'}/api/soldout`
-  const urlSoldOut = `http://3.87.44.241:8080/api/soldout`
+  const urlSoldOut = `http://44.208.37.247:8080/api/soldout`
   console.log('urlSoldOut', urlSoldOut)
 
   const { data } = await axios.get(urlSoldOut)
